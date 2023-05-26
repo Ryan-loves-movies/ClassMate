f992f6448febfffe4fe2d841c3204adf4ee92ea6
Ryan Tan <ryantan@Ryans-MacBook-Air.local>
2023-05-26 17:32:02 +0800

    dk what i changed in auth.tsx, doesn't matter

--- classmate/src/middleware/auth.tsx
+++ classmate/src/middleware/auth.tsx
@@ -1,26 +1,26 @@
 import { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
-// import config from '../config';
+import config from './config';
 import { NextResponse } from 'next/server';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function authenticateToken(req: NextApiRequest, res: NextApiResponse, next: any) {
-  const token = req.headers.authorization;
+    const token = req.headers.authorization;
 
-  if (!token) {
-    return res.status(401).json({ message: 'Unauthorized' });
-  }
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
 
-  try {
-    // const decoded = jwt.verify(token, config.JWT_SECRET);
-    // req.user = decoded.user;
-    next();
-  } catch (err) {
-    return res.status(401).json({ message: 'Invalid token' });
-  }
+    try {
+        const decoded = jwt.verify(token, config.JWT_SECRET);
+        // req.user = decoded.user;
+        next();
+    } catch (err) {
+        return res.status(401).json({ message: 'Invalid token' });
+    }
 }
 
 module.exports = {
-  authenticateToken,
+    authenticateToken,
 };
 
