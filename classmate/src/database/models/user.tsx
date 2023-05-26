afe20096987c220c3e2f854a68cb7b8b983c68cd
Ryan Tan <ryantan@Ryans-MacBook-Air.local>
2023-05-26 20:48:42 +0800

    removed unnecessary blank line at end of config.tsx file + Formatted user.tsx file properly

--- classmate/src/database/models/user.tsx
+++ classmate/src/database/models/user.tsx
@@ -2,23 +2,23 @@ import sequelize from '@/database/connection';
 import { DataTypes } from 'sequelize';
 
 const User = sequelize.define('users', {
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    validate: {
-      isEmail: true,
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
     },
-  },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true,
+        },
+    },
 });
 
 export default User;
