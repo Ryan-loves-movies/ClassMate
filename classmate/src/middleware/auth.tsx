9284880c686ea524916675a99e2b6053368171a1
Ryan Tan <ryantan@Ryans-MacBook-Air.local>
2023-05-26 18:53:53 +0800

    changed authorisation back to authorization and removed declare module 'express' in express-server.js file

--- classmate/src/middleware/auth.tsx
+++ classmate/src/middleware/auth.tsx
@@ -4,40 +4,24 @@ import { JwtPayload } from 'jsonwebtoken';
 import config from '@/config';
 import { NextFunction } from 'express';
 
-declare module 'express' {
-  interface Request {
-    headers: any;
-    user: any;
-  }
-  interface Response {
-    status: any;
-  }
-}
-
-interface Config {
-  JWT_SECRET: string;
-  // Other properties...
-}
-
 
 export default function authenticateToken(req: Request, res: Response, next: NextFunction) {
-  const token = req.headers.get('authorization');
+    const token = req.headers.authorisation;
 
-  if (!token || !token.trim()) {
-    return res.status(401).json({ message: 'Unauthorized' });
-  }
+    if (!token || !(token as string).trim()) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
 
-  try {
-    const decoded = jwt.verify(token, config.JWT_SECRET);
-    // Commented out bc user property is only defined on the JwtPayload interface
-    //req.user = decoded.user;
-    req.user = decoded as JwtPayload;
-    next();
-  } catch (err) {
-    return res.status(401).json({ message: 'Invalid token' });
-  }
+    try {
+        const decoded = jwt.verify(token as string, config.JWT_SECRET);
+        // Commented out bc user property is only defined on the JwtPayload interface
+        // req.user = decoded as JwtPayload;
+        next();
+    } catch (err) {
+        return res.status(401).json({ message: 'Invalid token' });
+    }
 }
 
 module.exports = {
-  authenticateToken,
+    authenticateToken,
 };
