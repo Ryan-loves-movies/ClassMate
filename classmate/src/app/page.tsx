a555a302a4c826adc54e64579be2327cf66ef3d5
Ryan Tan <ryantan@Ryans-MacBook-Air.local>
2023-05-27 10:38:22 +0800

    Modified handleLogin() function to use axios for api calls.

--- classmate/src/app/page.tsx
+++ classmate/src/app/page.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import LoginField from '@/components/LoginField';
-import jwt from 'jsonwebtoken';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 
 export default function Home() {
 
@@ -14,31 +14,47 @@ export default function Home() {
             const username = (event.target as HTMLFormElement).username;
             const password = (event.target as HTMLFormElement).password;
 
-            const requestOptions = {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    username,
-                    password,
-                }),
-            };
+            // const requestOptions = {
+            //     method: "POST",
+            //     headers: {
+            //         "Content-Type": "application/json",
+            //     },
+            //     body: JSON.stringify({
+            //         username,
+            //         password,
+            //     }),
+            // };
 
-            fetch("http://localhost:8000/auth/login", requestOptions)
-                .then((response) => {
-                    if (response.status === 200) {
-                        // ?? is the nullish coalescing operator
-                        // ?? will return value of Authorization header if not null, and return empty string if null or undefined 
-                        const token = response.headers.get("Authorization") ?? "";
-                        sessionStorage.setItem("token", token);
+            return axios.post("http://localhost:8000/login", {
+                username: username,
+                password: password
+            })
+                .then((res: AxiosResponse) => {
+                    if (res.status === 200) {
+                        sessionStorage.setItem("token", res.data.get("token"));
+                        window.location.href = '/authorized/dashboard';
                     } else {
                         alert("Invalid username or password");
                     }
                 })
-                .catch((error) => {
-                    console.error(error);
+                .catch((err: AxiosError) => {
+
+                    console.error(err)
                 });
+            // fetch("http://localhost:8000/login", requestOptions)
+            // .then((response) => {
+            //     if (response.status === 200) {
+            //         // ?? is the nullish coalescing operator
+            //         // ?? will return value of Authorization header if not null, and return empty string if null or undefined 
+            //         const token = response.headers.get("Authorization") ?? "";
+            //         sessionStorage.setItem("token", token);
+            //     } else {
+            //         alert("Invalid username or password");
+            //     }
+            // })
+            // .catch((error) => {
+            //     console.error(error);
+            // });
         };
     };
 
@@ -49,7 +65,7 @@ export default function Home() {
             <img src="/nusmods.png" className="flex-col flex ml-auto mr-auto items-center top" />
             <div className="flex-col flex ml-auto mr-auto items-center w-full lg:w-2/3 md:w-3/5 top">
                 <h1 className="font-bold text-10xl my-10 text-white"> Login </h1>
-                <form action="localhost:8000/users" className="mt-2 flex flex-col lg:w-1/2 w-8/12"> 
+                <form action="localhost:8000/users" className="mt-2 flex flex-col lg:w-1/2 w-8/12">
                     <LoginField type="text" placeholder="Username" name="username" leftIcon="bi bi-person-circle" rightIcon="" />
                     <LoginField type="password" placeholder="Password" name="password" leftIcon="bi bi-key" rightIcon="bi bi-eye-slash-fill" />
                     <div className="flex flex-wrap justify-between">
