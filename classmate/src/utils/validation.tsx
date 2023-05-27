c5a33ccfa0cb09eca7d531c8f30788ac79d93c2d
Ryan Tan <ryantan@Ryans-MacBook-Air.local>
2023-05-27 14:17:50 +0800

    Fixed indenting and exported regExp

--- classmate/src/utils/validation.tsx
+++ classmate/src/utils/validation.tsx
@@ -1,49 +1,38 @@
-// function isValidEmail(email) {
-//   // Email validation logic
-// }
-
-// function isValidPassword(password) {
-//   // Password validation logic
-// }
-
-// module.exports = {
-//   isValidEmail,
-//   isValidPassword,
-// };
-
-const regUsername = /^[a-z][^\W_]{7,14}$/i;
-const regPassword = /^(?=[^a-z]*[a-z])(?=\D*\d)[^:&.~\s]{5,20}$/;
-const regEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+const regUsername: RegExp = /^[a-z][^\W_]{7,14}$/i;
+const regPassword: RegExp = /^(?=[^a-z]*[a-z])(?=\D*\d)[^:&.~\s]{5,20}$/;
+const regEmail: RegExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
 function isValidUsername(username: string) {
-  return regUsername.test(username);
+    return regUsername.test(username);
 }
 
 function isValidPassword(password: string) {
-  return regPassword.test(password);
+    return regPassword.test(password);
 }
 
 function isValidEmail(email: string) {
-  return regEmail.test(email);
+    return regEmail.test(email);
 }
 
 function validateUser(username: string, password: string, email: string) {
-  if (!isValidUsername(username)) {
-    console.log("Invalid username.");
-    return false;
-  }
+    if (!isValidUsername(username)) {
+        console.log("Invalid username.");
+        return false;
+    }
 
-  if (!isValidPassword(password)) {
-    console.log("Invalid password.");
-    return false;
-  }
+    if (!isValidPassword(password)) {
+        console.log("Invalid password.");
+        return false;
+    }
 
-  if (!isValidEmail(email)) {
-    console.log("Invalid email address.");
-    return false;
-  }
+    if (!isValidEmail(email)) {
+        console.log("Invalid email address.");
+        return false;
+    }
 
-  console.log("User is valid.");
-  return true;
+    console.log("User is valid.");
+    return true;
 }
 
+export { regUsername, regPassword, regEmail };
+export default validateUser;
