ecae2517a6054f333b533e4fe1bad6745502ab90
Ryan Tan <ryantan@Ryans-MacBook-Air.local>
2023-05-27 10:24:06 +0800

    retyped express-server.js to express-server.ts to use typescript for consistency (File should be .ts not .tsx since .tsx is for files with JSX modules). tsconfig.json is untouched other than for formatting purposes. tsconfig-express.json is extended from tsconfig.json to be used for config file for the express-server files.

--- classmate/src/express-server.ts
+++ classmate/src/express-server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
-import expressRouter from '@/routes/userRoutes';
-import sequelize from '@/database/connection';
+import userRouter from './routes/userRoutes';
+import authRouter from './routes/authenticateUser';
+import sequelize from './database/connection';
 
 const expressApp = express();
 const port = 8000;
 
-expressApp.use('/', expressRouter);
+expressApp.use('/', userRouter);
+expressApp.use('/', authRouter);
 
 expressApp.listen(port, () => {
     console.log(`Server is running on port ${port}`);
@@ -16,5 +18,3 @@ process.on('SIGINT', () => {
     sequelize.close();
     process.exit();
 });
-
-export default expressApp;
