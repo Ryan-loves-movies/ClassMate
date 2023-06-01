a0ec4aa297336205f56b9221c1a924c8b2596537
Ryan Tan <ryantan@Ryans-MacBook-Air.local>
2023-06-01 19:23:15 +0800

    Modified Authentication component so that the unauthorised user page is not unintentionally shown when rendering page

--- nextjs/src/components/Authentication.tsx
+++ nextjs/src/components/Authentication.tsx
@@ -9,42 +9,31 @@ interface childrenElems {
 export default function AuthenticationComponent({ children }: childrenElems): ReactElement {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-    useEffect(() => {
-        const authenticateUser = async () => {
-            try {
-                // Perform the authentication logic, e.g., send JWT token to the server
-                // and validate it
-                axios.get(`${config.expressHost}/authenticate`, {
-                    headers: {
-                        Authorization: sessionStorage.getItem("token")
+    const authenticateUser = () => {
+        try {
+            // Perform the authentication logic, e.g., send JWT token to the server
+            // and validate it
+            axios.get(`${config.expressHost}/authenticate`, {
+                headers: {
+                    Authorization: sessionStorage.getItem("token")
+                }
+            })
+                .then((res: AxiosResponse) => {
+                    if (res.status === 200) {
+                        // If the authentication is successful, set isAuthenticated to true
+                        setIsAuthenticated(true);
+                    } else {
+                        setIsAuthenticated(false);
+                        window.location.href = '/';
+                        console.log('Authorization error', res.statusText);
                     }
-                })
-                    .then((res: AxiosResponse) => {
-                        if (res.status === 200) {
-                            // If the authentication is successful, set isAuthenticated to true
-                            setIsAuthenticated(true);
-                        } else {
-                            setIsAuthenticated(false);
-                            window.location.href = '/';
-                            console.log('Authorization error', res.statusText);
-                        }
-                    });
-            } catch (error) {
-                console.error('Axios authentication error:', error);
-            }
-        };
+                });
+        } catch (error) {
+            console.error('Axios authentication error:', error);
+        }
+    };
 
-        authenticateUser();
-    }, []);
-
-    if (!isAuthenticated) {
-        // Render a loading state or a component for unauthorized access
-        return (
-            <div>
-                <h1>You are an unauthorised user!</h1>
-            </div>
-        );
-    }
+    authenticateUser();
 
     // Render the children components if the user is authenticated
     return <>{children}</>;
