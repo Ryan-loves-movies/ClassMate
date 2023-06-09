3a9a650b13cd93d300674c8b1fb08bfef1dea2fb
Ryan Tan <ryantan@Ryans-MacBook-Air.local>
2023-06-09 12:21:45 +0800

    Finally refactored all the css files to .module.css and appropriately split the code for modularity.

--- nextjs/src/components/login/Error.tsx
+++
@@ -1,7 +0,0 @@
-export default function Error({ message }: { message: string }) {
-    return (
-        <div className="rounded  border border-red-600 bg-red-50 p-1 text-red-600">
-            {message}
-        </div>
-    );
-}
