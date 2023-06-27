67872e535b126290513490ed56c644e5128089b4
Ryan Tan <ryantan@Ryans-MacBook-Air.local>
2023-06-27 18:58:56 +0800

    Moved the following addProjectButton component to dashboard page instead of layout

--- nextjs/src/components/dashboard/AddProjectButton.tsx
+++ nextjs/src/components/dashboard/AddProjectButton.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import styles from "@components/dashboard/layout/addProjectButton.module.css";
+import styles from "@components/dashboard/addProjectButton.module.css";
 
-export default function AddProjectButton() {
+export default function AddProjectButton({ clickHandler }: { clickHandler: () => void }) {
     return (
-        <button className={styles['add-btn']} title="Add New Project" type="button">
+        <button className={styles['add-btn']} title="Add New Project" type="button" onClick={clickHandler}>
             <svg className="feather feather-plus" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round">
                 <line x1="12" y1="5" x2="12" y2="19" />
                 <line x1="5" y1="12" x2="19" y2="12" />
