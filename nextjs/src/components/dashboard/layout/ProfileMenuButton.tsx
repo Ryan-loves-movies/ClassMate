bd31c82f4982105ffb3d012c3004f089d941b5f1
Ryan Tan <ryantan@Ryans-MacBook-Air.local>
2023-06-29 14:14:40 +0800

    Modified profile menu to fetch user profile photo on load

--- nextjs/src/components/dashboard/layout/ProfileMenuButton.tsx
+++ nextjs/src/components/dashboard/layout/ProfileMenuButton.tsx
@@ -1,42 +1,47 @@
 'use client'
 import React, { useLayoutEffect, useState } from "react";
-// import axios, { AxiosError, AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
 import styles from '@components/dashboard/layout/profileMenu.module.css';
 import SignOut from '@components/dashboard/layout/SignOut';
 import Link from "next/link";
-// import config from '@/config';
+import config from '@/config';
+const { expressHost } = config;
 
 export default function ProfileMenuButton() {
     const [username, setUsername] = useState<string | null>();
+    const [photo, setPhoto] = useState<string>("");
     useLayoutEffect(() => {
         setUsername(window['sessionStorage'].getItem("username"));
+        function bufferToBase64(arr: []) {
+            return btoa(
+                arr.reduce((data: string, byte: number) => data + String.fromCharCode(byte), '')
+            );
+        }
+        axios.get(`${expressHost}/authorized/profile`, {
+            headers: {
+                Authorization: window['sessionStorage'].getItem("token")
+            },
+            params: {
+                username: window['sessionStorage'].getItem("username")
+            }
+        })
+            .then((res: AxiosResponse) => {
+                if (res.status === 200) {
+                    setPhoto(`data:image/png;base64,${bufferToBase64(res.data.photo.data)}`)
+                }
+            })
+            .catch((err: AxiosError) => {
+                alert("Sorry! A problem occured! Your email could not be found.");
+                console.error(err)
+            });
+        return () => URL.revokeObjectURL(photo);
     }, []);
-    // let [email, setEmail] = useState("");
-    // axios.get(`${config.expressHost}/profile`, {
-    //     headers: {
-    //         Authorization: window['sessionStorage'].getItem("token")
-    //     },
-    //     params: {
-    //         username: username
-    //     }
-    // })
-    //     .then((res: AxiosResponse) => {
-    //         if (res.status === 200) {
-    //             console.log(res.data);
-    //             let { email: tempRes } = res.data;
-    //             setEmail(tempRes);
-    //         }
-    //     })
-    //     .catch((err: AxiosError) => {
-    //         alert("Sorry! A problem occured! Your email could not be found.");
-    //         console.error(err)
-    //     });
     const [isDropdownActive, setDropdownActive] = useState(false);
     const toggleDropdown = () => setDropdownActive(!isDropdownActive);
 
     return (
         <button className={styles["profile-btn"]} onClick={toggleDropdown} type="button">
-            <img src="https://assets.codepen.io/3306515/IMG_2025.jpg" alt="Profile"/>
+            <img src={photo} alt="Profile" />
             <span>{username}</span>
             <div className={`${styles['dropdown-wrapper']} ${(isDropdownActive ? styles['active'] : '')}`} id='dropdownWrapper'>
                 <div className={styles['dropdown-profile-details']}>
