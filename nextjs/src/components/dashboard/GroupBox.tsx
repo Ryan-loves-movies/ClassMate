4c505e2144bb24a67107e80b4bc7f5536f1881a5
Ryan Tan <ryantan@Ryans-MacBook-Air.local>
2023-06-29 22:24:55 +0800

    Added new PhotoRenderer component that will return img react component of the photo with inputs of arrayBuffer and alt.

--- nextjs/src/components/dashboard/GroupBox.tsx
+++ nextjs/src/components/dashboard/GroupBox.tsx
@@ -1,10 +1,36 @@
 import React from "react";
 import styles from "@components/dashboard/groupBox.module.css";
+import PhotoRenderer from "./PhotoRenderer";
+
+interface user {
+    username: string;
+    photo: {
+        type: string,
+        data: Array<number>;
+    }
+}
 
 export default function GroupBox({
-    backgroundColor, header, subheader, width
+    backgroundColor, header, subheader, users, width
 }:
-    { backgroundColor: string, header: string, subheader: string, width: string }) {
+    {
+        backgroundColor: string,
+        header: string,
+        subheader: string,
+        users: user[],
+        width: string
+    }) {
+    const ProfilePhotos = ({ users }: { users: user[] }) => {
+        return (
+            <>
+                {users.map((user: user) => {
+                    return (
+                        <PhotoRenderer arrBuffer={user.photo.data} alt={user.username} key={user.username} />
+                    );
+                })}
+            </>
+        );
+    }
     return (
         <div className={styles['project-box-wrapper']} style={{ width: width }}>
             <div className={styles['project-box']} style={{ backgroundColor: backgroundColor }}>
@@ -26,8 +52,7 @@ export default function GroupBox({
                 </div>
                 <div className={styles['project-box-footer']}>
                     <div className={styles['participants']}>
-                        <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80" alt="participant" />
-                        <img src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTB8fG1hbnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60" alt="participant" />
+                        <ProfilePhotos users={users} />
                         <button className={styles['add-participant']} style={{ color: "#ff942e" }} type="button">
                             <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className='feather feather-plus'>
                                 <path d="M12 5v14M5 12h14" />
--- nextjs/src/components/dashboard/layout/ProfileMenuButton.tsx
+++ nextjs/src/components/dashboard/layout/ProfileMenuButton.tsx
@@ -5,18 +5,14 @@ import styles from '@components/dashboard/layout/profileMenu.module.css';
 import SignOut from '@components/dashboard/layout/SignOut';
 import Link from "next/link";
 import config from '@/config';
+import PhotoRenderer from "../PhotoRenderer";
 const { expressHost } = config;
 
 export default function ProfileMenuButton() {
     const [username, setUsername] = useState<string | null>();
-    const [photo, setPhoto] = useState<string>("");
+    const [photoArrBuffer, setPhotoArrBuffer] = useState<number[]>([]);
     useLayoutEffect(() => {
         setUsername(window['sessionStorage'].getItem("username"));
-        function bufferToBase64(arr: []) {
-            return btoa(
-                arr.reduce((data: string, byte: number) => data + String.fromCharCode(byte), '')
-            );
-        }
         axios.get(`${expressHost}/authorized/profile`, {
             headers: {
                 Authorization: window['sessionStorage'].getItem("token")
@@ -27,21 +23,20 @@ export default function ProfileMenuButton() {
         })
             .then((res: AxiosResponse) => {
                 if (res.status === 200) {
-                    setPhoto(`data:image/png;base64,${bufferToBase64(res.data.photo.data)}`)
+                    setPhotoArrBuffer(res.data.photo.data);
                 }
             })
             .catch((err: AxiosError) => {
                 alert("Sorry! A problem occured! Your email could not be found.");
                 console.error(err)
             });
-        return () => URL.revokeObjectURL(photo);
     }, []);
     const [isDropdownActive, setDropdownActive] = useState(false);
     const toggleDropdown = () => setDropdownActive(!isDropdownActive);
 
     return (
         <button className={styles["profile-btn"]} onClick={toggleDropdown} type="button">
-            <img src={photo} alt="Profile" />
+            <PhotoRenderer arrBuffer={photoArrBuffer} alt="Profile" />
             <span>{username}</span>
             <div className={`${styles['dropdown-wrapper']} ${(isDropdownActive ? styles['active'] : '')}`} id='dropdownWrapper'>
                 <div className={styles['dropdown-profile-details']}>
