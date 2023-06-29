61b158483ee7f7da05b1cc47128b16c75738ef0e
Ryan Tan <ryantan@Ryans-MacBook-Air.local>
2023-06-29 14:16:21 +0800

    Toggling change again for commiting purposes

--- server/database/connection.tsx
+++ server/database/connection.tsx
@@ -5,13 +5,12 @@ const Sequelize = require('sequelize');
     port: 6543,
     dialect: 'postgres'
 });
-*/
-
+ */
 const sequelize = new Sequelize('classmate', 'root', 'Classmate123!', {
     host: '127.0.0.1',
     port: 3306,
     dialect: 'mysql'
-})
+}) 
 export function createSequelizeConnection(instance: typeof Sequelize) {
     try {
         instance.authenticate();
