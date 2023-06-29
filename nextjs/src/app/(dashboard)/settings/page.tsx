17cc0be6f01b9405bcb5fab38cadf5aba370f847
Ryan Tan <ryantan@Ryans-MacBook-Air.local>
2023-06-29 14:13:48 +0800

    Added 2 new sections on the settings page - one to update the database, another to update the profile photo for the user

--- nextjs/src/app/(dashboard)/settings/page.tsx
+++ nextjs/src/app/(dashboard)/settings/page.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from "@app/(dashboard)/settings/settings.module.css";
+import DevButtons from "@components/dashboard/DevButtons";
+import ProfilePhotoButton from "@components/dashboard/ProfilePhotoButton";
 
 export default function timetable() {
     return (
@@ -10,6 +12,24 @@ export default function timetable() {
             <div className={styles['settings-box']}>
                 <h3 className={styles['settings-header']}>Reset Password</h3>
                 <span className={styles['settings-description']}>Reset your password</span>
+            </div>
+            <div className={styles['settings-box']}>
+                <h3 className={styles['settings-header']}>Update Profile Photo</h3>
+                <span className={styles['settings-description']}>
+                    <span>Update your profile photo!</span>
+                    <ProfilePhotoButton />
+                    </span>
+            </div>
+            <div className={styles['settings-box']}>
+                <h3 className={styles['settings-header']}>Developer Options</h3>
+                <span className={styles['settings-description']}>
+                    <span>
+                        Update modules and lessons in database
+                    </span>
+                    <div className={styles['action']}>
+                        <DevButtons />
+                    </div>
+                </span>
             </div>
         </div>
     );
